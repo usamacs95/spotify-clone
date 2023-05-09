481cd7a12f79ec99672cb0f0c3390da742ea4ae5
Usama Qazi <usama_cs95@outlook.com>
2023-05-09 13:42:46 +0500

    trying to play first song.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Welcome to Spot on!");
+
+// Initalize the variables
+let songIndex
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById(masterPlay);
+let myProgressBar = document.getElementById(myProgressBar);
+let songs = [
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
+]
+// audioElement.play();
+
+//Here we will control play/pause on click button
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0)
+    {
+        audioElement.play();
+    }
+})
+// Event listeners
+myProgressBar.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    //here we are updating the seekbar.
+});
