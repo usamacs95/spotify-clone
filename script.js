c4fdc4dedb4b34764358d3f3cea7c1e780779c9e
Usama Qazi <usama_cs95@outlook.com>
2023-05-09 16:37:56 +0500

    play, pause working

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log("Welcome to Spot on!");
 // Initalize the variables
 let songIndex
 let audioElement = new Audio('songs/1.mp3');
-let masterPlay = document.getElementById(masterPlay);
-let myProgressBar = document.getElementById(myProgressBar);
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 let songs = [
     {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
     {songName: "Perfect", filePath: "songs/1.mp3", coverPath:"covers/1.jpg"},
@@ -24,10 +25,22 @@ masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0)
     {
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle')
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle')
+        gif.style.opacity = 0;
     }
 })
 // Event listeners
-myProgressBar.addEventListener('timeupdate', ()=>{
+audioElement.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     //here we are updating the seekbar.
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+    console.log(progress);
+    
 });
